3d01ba0cf1dd19332e7c50183f9bea5b05eace6c
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-01-04 16:11:49 -0500

    (0x03) task 2
    Created Fabric manifest for task 2.

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Fabric file for task 2.
+"""
+import os
+from fabric.api import *
+
+
+env.key_filename = "~/.ssh/holberton"
+env.user = "ubuntu"
+env.hosts = ['35.237.10.249', '35.237.191.203']
+
+
+def do_deploy(archive_path):
+    """
+    Deploys the contents of a web_static TGZ into a production server.
+    """
+
+    if not os.path.exists(archive_path):
+        return False
+
+    filename = os.path.basename(archive_path)
+    put(archive_path, "/tmp/")
+
+    run("mkdir -p /data/web_static/releases/{}".format(filename.split(".")[0]))
+    if run("tar -xzf \"/tmp/{}\" -C /data/web_static/releases/{}".format(
+            filename,
+            filename.split(".")[0])).failed:
+        return False
+
+    if run("rm /tmp/{}".format(filename)).failed:
+        return False
+
+    if run("rm /data/web_static/current").failed:
+        return False
+
+    if run("ln -sf /data/web_static/releases/{} "
+           "/data/web_static/current".format(
+               filename.split(".")[0])).failed:
+        return False
+
+    return True
