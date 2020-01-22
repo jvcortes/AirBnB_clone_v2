016489b098c07362021a5948d5a210d725085473
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-01-22 14:49:48 -0500

    (web_flask) task 3
    Created Python script for task 3.

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# Runs a Flask web application with four defined routes.
+from flask import Flask, escape
+
+
+app = Flask('__main__')
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """
+    Defines a route with a simple response.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    Defines a route with a simple response.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """
+    Defines a route with a URL-parameter defined response.
+
+    Args:
+        text (str): text to display.
+    """
+    return "C {}".format(escape(text.replace('_', ' ')))
+
+
+@app.route('/python/', defaults={"text": "is cool"})
+@app.route('/python/<text>')
+def python_is_cool(text):
+    """
+    Defines a route with a URL-parameter (or default) defined response.
+
+    Args:
+        text (str): text to display.
+    """
+    return "Python {}".format(escape(text.replace('_', ' ')))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
